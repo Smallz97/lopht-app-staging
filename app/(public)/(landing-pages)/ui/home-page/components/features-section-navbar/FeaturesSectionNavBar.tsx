68f3bc9bcad703e570beb683aa55946979618102
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2024-11-01 15:02:10 +0100

    minor UI changes for preview (features-section-navbar)

--- app/(public)/(landing-pages)/ui/home-page/components/features-section-navbar/FeaturesSectionNavBar.tsx
+++ app/(public)/(landing-pages)/ui/home-page/components/features-section-navbar/FeaturesSectionNavBar.tsx
@@ -1,8 +1,8 @@
 export default function FeaturesSectionNavigationBar() {
   return (
     <div className="lg:px-64" id="features-navbar">
-      <div className="bg-slate-50 rounded-full flex lg:gap-0.5">
-        <div className="py-5 rounded-full border border-green-600 justify-center items-center grow shrink basis-0 flex">
+      <div className="bg-slate-50 rounded-xl md:rounded-full flex lg:gap-0.5">
+        <div className="py-5 rounded-xl md:rounded-full border border-green-600 justify-center items-center grow shrink basis-0 flex">
           <div className="text-black text-xs md:text-lg font-medium leading-none">
             Administrators
           </div>
@@ -12,8 +12,8 @@ export default function FeaturesSectionNavigationBar() {
             Communities
           </div>
         </div>
-        <div className="py-5 rounded-full justify-center items-center grow shrink basis-0 flex">
-          <div className="text-black text-xs md:text-lg font-normal leading-none">
+        <div className="py-5 rounded-xl md:rounded-full justify-center items-center grow shrink basis-0 flex">
+          <div className="text-black text-xs md:text-lg font-normal leading-none ">
             Events
           </div>
         </div>
